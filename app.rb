562b27375ea847b7da4498c3336975c4b8b63da7
Abdullah Rashed <abdullah_rashed.a@icloud.com>
2019-11-11 22:30:01 +0300

    lab

---
+++ app.rb
@@ -0,0 +1,91 @@
+# Run this code
+
+# Ask the user for stuff...
+
+#puts "Welcom to the Camel Rattler! How tall are you (in feet)?"
+
+# height = gets .chomp.to_i
+
+# if height < 4
+#    puts "Sorry, you'll fly out of your seat if we let you on."
+# elsif height <7 
+#    puts 'All aboard!'
+# else puts "If you value your head, you should not get on this ride"
+# end
+
+# user_input = gets.chomp.to_i
+# if user_input > 10
+#    puts "#You are older than ten!"
+# end
+
+# num_of_wheels = 1
+# case num_of_wheels
+# when 1
+#     p'Unicycle'
+# when 2
+#     p'Bicycle'
+# when 4
+#     p'Car'
+# else
+#     p'Im not sure, maybe a duck?'
+# end
+#person_name ='Hazim'
+#case person_name
+
+#sei_class = {
+#    teacher: 'Usman',
+#    students: [1,2,4],
+#    classroom:42,
+#    in_session:true
+#}
+
+#p sei_class
+
+#def double(number)
+#    doubled_number = number * 2
+#    return doubled_number
+#end
+
+#p double 5
+
+# def hello(name, greeting, small_talk, punctuation)
+#     return "#{greeting}#{name}#{small_talk}#{punctuation}"
+# end
+
+# p hello("Usman ", "Hi ", "you suck", "!")
+
+# def drink_milk(thirsty = true)
+#     return "I'm not thirsty" unless thirsty == false
+#     "mmmmm....milk...."
+# end
+
+# p drink_milk(false)
+
+# users = ['Sarah Alghabbamy', 'Abdulrahman Alshabibi', 'Noura Alnuukhait']
+# for user in users do
+#     puts user
+# end
+#arr = [10, 10, 30]
+
+# arr.ForEach(function(item, index) {})
+#arr.forEach( (num) => {somecode} )
+
+#arr.each{ |num| puts num }
+
+
+# arr.each do |num|
+#     puts num
+# end
+# i = 1
+# while i <= 100
+#     i += 1 
+#     if i % 3 == 0 && i % 5  == 0
+#         p "FizzBuzz"
+#     elsif i % 5 == 0
+#         p "Buzz"
+#     elsif i % 3 == 0
+#         p "Fizz"
+#     else
+#         p i
+# end
+# end
